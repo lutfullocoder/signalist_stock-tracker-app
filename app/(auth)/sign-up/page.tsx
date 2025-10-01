bf7eb419c9ccee2ec872d48ae9564ae347d5c6d7
lutfullocoder <lutfulloa02@gmail.com>
2025-10-01 20:52:47 +0500

    implement auth logic

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -7,8 +7,13 @@ import SelectField from "@/components/forms/SelectField";
 import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
 import {CountrySelectField} from "@/components/forms/CountrySelectField";
 import FooterLink from "@/components/forms/FooterLink";
+import {signUpWithEmail} from "@/lib/actions/auth.actions";
+import {useRouter} from "next/navigation";
+import {toast} from "sonner";
 
 const SignUp = () => {
+    const router = useRouter();
+
     const {register, handleSubmit, control, formState: {errors, isSubmitting}} = useForm<SignUpFormData>(
         {
             defaultValues: {
@@ -24,13 +29,19 @@ const SignUp = () => {
         }
     )
 
-    const onSubmit: SubmitHandler<SignUpFormData> = (data) => {
+    const onSubmit: SubmitHandler<SignUpFormData> = async (data) => {
         try {
-            console.log(data)
+           const result = await signUpWithEmail(data)
+
+            if(result.success) router.push("/")
         } catch (error) {
             console.log(error)
+            toast.error('Sign up failed', {
+                description: error instanceof Error ? error.message : "Failed to create an account",
+            });
         }
     }
+
     return (
         <>
             <h1 className={'form-title'}>Sign Up & Personalize</h1>
